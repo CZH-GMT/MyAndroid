a9fbfe4be79dac255de9ac9a09f3b8bc66cb45e6
chenzhihua <chenzhihua@faw.com.cn>
2022-12-27 15:31:44 +0800

    fix

--- javaIO/src/main/java/classTop/IoUtil.java
+++ javaIO/src/main/java/classTop/IoUtil.java
@@ -7,17 +7,19 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 
 public class IoUtil {
 
-    private static String path="/usr/local/src/project/MyApplication2/app/src/main/java/com/example/myapplication2/io/soutfile";
+    private static String path = "/usr/local/src/project/MyApplication2/app/src/main/java/com/example/myapplication2/io/soutfile";
 
     /**
-     *
      * @param filePath
      * @return
      */
@@ -37,7 +39,6 @@ public class IoUtil {
     }
 
     /**
-     *
      * @param i
      * @return
      */
@@ -46,14 +47,13 @@ public class IoUtil {
     }
 
     /**
-     *
      * @param filePath
      * @param t
      * @return
      */
-    public static  boolean writeFile(String filePath, String t) {
+    public static boolean writeFile(String filePath, String t) {
         try {
-            BufferedWriter fileWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream( filePath)));
+            BufferedWriter fileWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath)));
             fileWriter.write(t);
             fileWriter.flush();
             fileWriter.close();
@@ -65,12 +65,11 @@ public class IoUtil {
     }
 
     /**
-     *
      * @param filePath
      * @param s
      * @return
      */
-    public static boolean writeInputStream(String filePath,String s){
+    public static boolean writeInputStream(String filePath, String s) {
         try {
             OutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(filePath));
             bufferedOutputStream.write(s.getBytes(StandardCharsets.UTF_8));
@@ -84,17 +83,71 @@ public class IoUtil {
     }
 
     /**
+     * @param filePath
+     * @param obj
+     * @return
+     */
+    public static boolean writeObject(String filePath, Object obj) {
+        ObjectOutputStream objectOutputStream = null;
+        if (obj == null) {
+            return false;
+        }
+        try {
+            objectOutputStream = new ObjectOutputStream(new FileOutputStream(filePath));
+            objectOutputStream.writeObject(obj);
+            objectOutputStream.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (objectOutputStream != null) {
+                    objectOutputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
      *
+     * @param filePath
+     * @param <T>
+     * @return
+     */
+    public static <T> T readObject(String filePath) {
+        ObjectInputStream objectInputStream = null;
+        try {
+            objectInputStream = new ObjectInputStream(new FileInputStream(filePath));
+            return (T) objectInputStream.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (objectInputStream != null) {
+                try {
+                    objectInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+
+    }
+
+    /**
      * @param olePath
      * @param newFile
      * @return
      */
-    public static boolean copyOldToNew(String olePath,String newFile){
+    public static boolean copyOldToNew(String olePath, String newFile) {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(olePath));
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(newFile));
             char[] chars = new char[1024];
-            while (bufferedReader.read(chars)!=-1){
+            while (bufferedReader.read(chars) != -1) {
                 bufferedWriter.write(chars);
             }
             bufferedWriter.flush();
@@ -114,6 +167,6 @@ public class IoUtil {
 //        System.out.println(writeFile(path+"/xxxx.txt", "2232323"));
 //        System.out.println(writeInputStream(path + "/xxxx.txt","wertyu9odfjfvusahndvr8e73q4enfudhcvquievcndvadfvwdfjvaionviqwfdivndfjvans"));
 
-        copyOldToNew(path+"/xxxx.txt",path+"/oooo.txt");
+        copyOldToNew(path + "/xxxx.txt", path + "/oooo.txt");
     }
 }
--- kotlin/src/main/java/com/example/kotlin/KotlinShuangJianShuoDanLi.kt
+++ kotlin/src/main/java/com/example/kotlin/KotlinShuangJianShuoDanLi.kt
@@ -3,10 +3,14 @@ package com.example.kotlin
 //TODO kotlin 双检锁单例
 class KotlinShuangJianShuoDanLi private constructor() { //加上 private constructor 就不能调用这里了只能调用下面
     companion object {
-        val instance: KotlinShuangJianShuoDanLi by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) {
-            KotlinShuangJianShuoDanLi()
-        }
-//            get() {
+//        val instance: KotlinShuangJianShuoDanLi by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) {
+//            KotlinShuangJianShuoDanLi()
+//        }
+
+//        //Todo:懒汉试
+//        var instance: KotlinShuangJianShuoDanLi? = null
+//
+//        get() {
 //                if (instance == null) {
 //                    instance = KotlinShuangJianShuoDanLi()
 //                }
@@ -17,6 +21,6 @@ class KotlinShuangJianShuoDanLi private constructor() { //加上 private constru
 }
 
 fun main() {
-    KotlinShuangJianShuoDanLi.instance.show()
+//    KotlinShuangJianShuoDanLi.instance.show()
 
 }
